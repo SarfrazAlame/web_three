10e5b761d15e64594414d3c639e17fe394866041
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-09-05 05:51:54 +0530

    kdlf

--- index.ts
+++ index.ts
@@ -37,15 +37,15 @@
 
 
 ////////////////////////////////////////////// converting Array to hex
-function arrayToHex(byteArray) {
-  let hexString = '';
-  for (let i = 0; i < byteArray.length; i++) {
-    hexString += byteArray[i].toString(16).padStart(2, '0');
-  }
-  return hexString;
-}
+// function arrayToHex(byteArray) {
+//   let hexString = '';
+//   for (let i = 0; i < byteArray.length; i++) {
+//     hexString += byteArray[i].toString(16).padStart(2, '0');
+//   }
+//   return hexString;
+// }
 
-// Example usage:
-const byteArray = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
-const hexString = arrayToHex(byteArray);
-console.log(hexString);  
+// // Example usage:
+// const byteArray = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+// const hexString = arrayToHex(byteArray);
+// console.log(hexString);  
